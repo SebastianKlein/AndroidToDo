94e7cb6e0f4d42f2b6e7e107857b039709a3978f
PHACRUM <moritz.klein@protonmail.com>
2016-03-17 19:34:27 +0100

    update
    !

--- app/src/main/java/com/klein/todo/AccountSettingsActivity.java
+++ app/src/main/java/com/klein/todo/AccountSettingsActivity.java
@@ -1,5 +1,6 @@
 package com.klein.todo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,13 +10,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.klein.todo.database.DataSource;
+import com.klein.todo.model.User;
+
 public class AccountSettingsActivity extends AppCompatActivity {
 
+    private DataSource dataSource;
     private Button bChangePassword;
     private Button bAddAccount;
     private EditText etPassword;
     private EditText etNewPassword;
     private EditText etNewPasswordVerify;
+    private User currentUser; // = dataSource.getUser(id);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,52 @@ public class AccountSettingsActivity extends AppCompatActivity {
         bChangePassword = (Button) findViewById(R.id.bChangePassword);
         bAddAccount = (Button) findViewById(R.id.bAddAccount);
 
+        bChangePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String oldPassword = etPassword.getText().toString().trim();
+                String newPassword = etNewPassword.getText().toString().trim();
+                String newPasswordVerify = etNewPasswordVerify.getText().toString().trim();
 
+                if (currentUser.getPassword().equals(oldPassword) &&  // old passwords match
+                        etNewPassword.getText().toString().trim().equals(newPasswordVerify) && // new passwords match
+                        check_values(newPassword, newPasswordVerify)) { // new password follows rules
+                    currentUser.setPassword(newPassword);
+                }
+            }
+        });
+
+        bAddAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentUser.getPassword() == 0) {
+                    addUser();
+                }
+            }
+        });
+    }
+
+    private void addUser() {
+        Intent addUser_intent = new Intent(this, AddUserActivity.class);
+        startActivity(addUser_intent);
+    }
+
+    private boolean check_values(String password, String passwordVerify) {
+        if (password.equals(passwordVerify) && password.length() >= 5 && password.length() <= 20) {
+            int literal = interval(password, 'A', 'z');
+            int digit = interval(password, '0', '9');
+            if (digit > 0 && literal > 0 && password.length() - digit - literal == 0)
+                return true;
+        }
+        return false;
+    }
+
+    private int interval(String pass, char min, char max) {
+        int count = 0;
+        for (char c : pass.toCharArray()) {
+            if (c >= min && c <= max)
+                count++;
+        }
+        return count;
     }
 }
